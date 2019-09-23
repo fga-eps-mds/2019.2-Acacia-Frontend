f9836361b77cd54acfc4fec800d5803e25d9f403
RenatoBrittoAraujo <renatomwbbritto@gmail.com>
2019-09-23 12:32:54 -0300

    Setup basic filesystem

---
+++ src/router.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router({
+	routes: [
+		{
+			path: '/',
+			name: 'home',
+			component: () => import('./views/HomePage.vue')
+		},
+		{
+			path: '/signin',
+			name: 'signin',
+			component: () => import('./views/Signin.vue')
+		},
+		{
+			path: '/signup',
+			name: 'signup',
+			component: () => import('./views/Signup.vue')
+		}
+	]
+})
---
+++ src/views/Signin.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="signin">
+    <h1>This is an sign in page</h1>
+  </div>
+</template>
