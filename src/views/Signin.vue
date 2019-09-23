0f95102a3e309fde76b11163f1eafa2a9925c188
RenatoBrittoAraujo <renatomwbbritto@gmail.com>
2019-09-23 12:40:57 -0300

    #44 Add basic template for login

--- src/views/Signin.vue
+++ src/views/Signin.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="signin">
-    <h1>This is an sign in page</h1>
+    <h1>Sign in</h1>
+    <form action="">
+      <label for="">Username or email</label>
+      <br>
+      <input type="text">
+      <br>
+      <label for="">Password</label>
+      <br>
+      <input type="">
+      <br>
+      <button type="submit">Sign in</button>
+    </form>
   </div>
 </template>
