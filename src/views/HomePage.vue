cce17c14ce5a1a8412398d061d78ce1d4b16a2d6
RenatoBrittoAraujo <renatomwbbritto@gmail.com>
2019-09-24 16:01:11 -0300

    #44 Add basic project structure for pages and css classes

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="home">
-    <p>Currently not logged in</p>
+    <h1 class="raleway-bold"> Essa é a página root </h1>
+		<p> Como não temos um protótipo de alta fidelidade para a versão desktop do site, a unica maneira de </p>
+		<p> navegar pelas páginas é alterando a url</p>
+		<p> no caso</p>
+		<p>/signin - para a página de login</p>
+		<p>/signup - para a página de sign</p>
+
   </div>
 </template>
 
--- src/views/Signin.vue
+++ src/views/Signin.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="signin">
-    <h1>This is an sign in page</h1>
+  <div class="signin gradient">
+		<div class="col"></div>
+		<h1 class="raleway-bold">This is an sign in page</h1>
   </div>
 </template>
+
+<style scoped>
+.signin {
+	width: 100%;
+	height: 100%;
+	position: absolute;
+	margin-top: 0;
+	text-align: center;
+}
+.gradient {
+	background-image: linear-gradient(180deg, rgba(86, 163, 166, 1), rgba(55, 105, 150, 200));
+}
+</style>
