72928ba27657279f223693c3faa381b18711a769
Hugo Sobral <hugosobral10@gmail.com>
2019-09-26 21:01:18 -0300

    #43 Add initial version of sign up page
    Co-authored-by: Leonardo da Silva Gomes <leonardodasigomes@gmail.com>

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,5 +1,77 @@
 <template>
-  <div class="signup">
-    <h1>This is an sign up page</h1>
+  <div class="signup gradient">
+    <TopBar :iconleft="'chevron-left'"></TopBar>
+    <div class="content-container">
+      <div class="content-title">
+        <h1> Create account </h1>
+      </div>
+      <div class="content-form">
+        <TextField class="mt-3" v-model="username" :label="'Name'"></TextField>
+        <TextField class="mt-3" v-model="email" :label="'Email'"></TextField>
+        <TextField class="mt-3" v-model="password" :label="'Password'" :password="true"></TextField>
+        <TextField class="mt-3" v-model="confirm_password" :label="'Confirm password'" :password="true"></TextField>
+      </div>
+      <div class="content-button">
+        <SignButton class="mt-4" :label="'Sign up'"></SignButton>
+      </div>
+    </div>
+
   </div>
 </template>
+
+<script>
+  import TopBar from '../components/layout/TopBar'
+  import TextField from '../components/input/TextField'
+  import SignButton from '../components/input/SignButton'
+
+  export default {
+    components: {
+      TopBar,
+      TextField,
+      SignButton,
+    },
+    data() {
+      return {
+        username: '',
+        email: '',
+        password: '',
+        confirm_password: '',
+      }
+    }
+  }
+
+</script>
+
+<style lang="scss" scoped>
+  @import "../assets/stylesheets/colors.scss";
+
+  .content-title {
+    width: 100%;
+    padding: 0px 20px;
+    margin-bottom: 20%;
+    color: $color-default-text;
+    display: flex;
+    justify-content: left; 
+  }
+  
+  .content-container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 100%;
+	}
+
+  .signup {
+    width: 100%;
+		height: 100%;
+		position: absolute;
+		margin-top: 0;
+		text-align: center;
+  }
+
+  .gradient {
+    background-image: linear-gradient(180deg, rgba(86, 163, 166, 1), rgba(75s, 125, 170, 105));
+  }
+</style>
