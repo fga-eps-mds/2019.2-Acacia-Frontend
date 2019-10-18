fe9093d6a8e022a0c50d03ea046d256baf8f04e2
Leonardo da Silva Gomes <leonardodasigomes@gmail.com>
2019-10-18 14:02:16 -0300

    #73 Create photoupload component

---
+++ src/components/input/PhotoUpload.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="fileupload">
+      <input style="display:none" ref="fileInput" type="file" @change="onFileSelected">
+      <button @click="$refs.fileInput.click()">Leo Lindo</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+            selectedFile: null
+        }
+    },
+    methods: {
+        onFileSelected(event){
+            this.$emit('action', this.selectedFile = event.target.files[0])
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
