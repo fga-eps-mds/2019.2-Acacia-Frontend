57c410cfe17954c77aca35547aaf1a06788c4c83
jps12 <jps12carvalho@gmail.com>
2019-10-18 18:06:15 -0300

    #73 Add Datepicker library
    Co-authored-by: Leonardo da Silva Gomes <leonardodasigomes@gmail.com>

--- src/main.js
+++ src/main.js
@@ -22,8 +22,12 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
+/*Datepicker configuration*/
+import Datepicker from 'vuejs-datepicker'
+
 library.add(fas)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.component('datepicker', Datepicker)
 
 Vue.config.productionTip = false
 
