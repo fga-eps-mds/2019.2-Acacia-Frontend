d389606ad3fd6f885d0896d738d6e5cfc702d32a
Vítor Xoteslem <vitorcx010@gmail.com>
2019-10-19 00:20:11 -0300

    #79 Add staging express server
    Co-authored-by: Shayane <shayaneliebe@gmail.com>

---
+++ staging-server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var port = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.static(__dirname + "/dist/"));
+app.get(/.*/, function(req, res){
+    res.sendfile(__dirname + "/dist/index.html");
+});
+app.listen(port);
