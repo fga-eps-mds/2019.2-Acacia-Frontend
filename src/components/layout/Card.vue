38ed378f074865bc75edd19e353b33a54d040ff6
Hugo Sobral <hugosobral10@gmail.com>
2019-10-22 19:51:24 -0300

    #116 Create initial structure of card component
    Co-authored-by: MarthaDs <marthadntas@gmail.com>

---
+++ src/components/layout/Card.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container">
+
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+    @import "../../assets/stylesheets/colors.scss";
+    
+    .container {
+        width: 300px;
+        height: 255px;
+        border-radius: 12px;
+        background-color: white;
+        border: none;
+        box-shadow: 0 2px 3px 0.5px $color-shadow;
+        outline: none;
+    }
+
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,5 +1,25 @@
 <template>
     <div class="dashboard">
-        dashboard
+        <Card/>
     </div>
 </template>
+
+<script>
+import Card from '@/components/layout/Card'
+
+export default {
+    components: {
+        Card,
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+    .dashboard {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        background-color: #F8F8FA;
+    }
+
+</style>
