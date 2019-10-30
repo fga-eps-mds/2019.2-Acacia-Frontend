8b6eab164b41e7edbb427e78d9fec34e94e16f7a
Durval Carvalho <durvalcsouza@outlook.com>
2019-10-30 16:57:02 -0300

    #74 Fix syntax errors
    Co-authored-by: Leonardo da Silva Gomes <leonardodasigomes@gmail.com>

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,40 +1,38 @@
 <template>
   <div class="signup gradient">
-    <TopBar
-    : iconleft="'chevron-left'"
-    />
+    <TopBar :iconleft="'chevron-left'" />
     <div class="content-container">
       <div class="content-title">
         <a style="font-size: 40px">{{ $t('SignPages.createAccount') }}</a>
       </div>
       <div class="content-form">
         <TextField
-        v-model="username"
-        class="mt-3"
-        : label="this.$t('SignPages.name')"
+          v-model="username"
+          class="mt-3"
+          :label="this.$t('SignPages.name')"
         />
         <TextField
-        v-model="email"
-        class="mt-3"
-        : label="this.$t('SignPages.email')"
+          v-model="email"
+          class="mt-3"
+          :label="this.$t('SignPages.email')"
         />
         <TextField
-        v-model="password"
-        class="mt-3"
-        : label="this.$t('SignPages.password')"
-        :password="true"
+          v-model="password"
+          class="mt-3"
+          :label="this.$t('SignPages.password')"
+          :password="true"
         />
         <TextField
-        v-model="confirm_password"
-        class="mt-3"
-        : label="this.$t('SignPages.confirmPassword')"
-        :password="true"
+          v-model="confirm_password"
+          class="mt-3"
+          :label="this.$t('SignPages.confirmPassword')"
+          :password="true"
         />
       </div>
       <div class="content-button">
         <SignButton
-        class="mt-4"
-        : label="this.$t('SignPages.createAccount')"
+          class="mt-4"
+          :label="this.$t('SignPages.createAccount')"
         @action="signup"
         />
       </div>
@@ -43,114 +41,115 @@
 </template>
 
   <script>
-import TopBar from "@/components/layout/TopBar";
-import TextField from "@/components/input/TextField";
-import SignButton from "@/components/input/SignButton";
+    import TopBar from "@/components/layout/TopBar";
+    import TextField from "@/components/input/TextField";
+    import SignButton from "@/components/input/SignButton";
 
-/* Local scripts imports */
-import router from "@/router";
+    /* Local scripts imports */
+    import router from "@/router";
 
-/* External library */
-import axios from "axios";
+    /* External library */
+    import axios from "axios";
 
 export default {
-  components: {
-    TopBar,
-    TextField,
-    SignButton
-  },
+      components: {
+      TopBar,
+      TextField,
+      SignButton
+    },
   data() {
     return {
       username: "",
-      email: "",
-      password: "",
-      confirm_password: ""
-    };
-  },
+    email: "",
+    password: "",
+    confirm_password: ""
+  };
+},
   methods: {
-    signup() {
+      signup() {
       if (!this.validateInput()) {
         return;
-      }
+  }
       let data = {
-        email: this.email,
-        username: this.username,
-        password: this.password,
-        confirm_password: this.confirm_password
-      };
+      email: this.email,
+    username: this.username,
+    password: this.password,
+    confirm_password: this.confirm_password
+  };
       let dataToken = {
-        email: this.email,
-        password: this.password
-      };
-      const baseURL = this.$store.state.baseURL;
-      axios
-        .post(baseURL + "users/signup/", data)
+      email: this.email,
+    password: this.password
+  };
+  const baseURL = this.$store.state.baseURL;
+  axios
+    .post(baseURL + "users/signup/", data)
         .then(() => {
-          axios
-            .post(baseURL + "users/token/", dataToken)
-            .then(response => {
-              this.$store.state.authUser(
-                response.data["access"],
-                response.data["refresh"]
-              );
-              this.$toasted
-                .show(this.$t("SignPages.positiveStatus"))
-                .goAway(2000);
-              router.push({ name: "home" });
-            })
-            .catch(() => {
-              router.push({ name: "signin" });
-            });
+      axios
+        .post(baseURL + "users/token/", dataToken)
+        .then(response => {
+          this.$store.state.authUser(
+            response.data["access"],
+            response.data["refresh"]
+          );
+          this.$toasted
+            .show(this.$t("SignPages.positiveStatus"))
+            .goAway(2000);
+          router.push({ name: "home" });
         })
+        .catch(() => {
+          router.push({ name: "signin" });
+        });
+})
         .catch(error => {
           if (error.response.data.email) {
-            this.$toasted.show(error.response.data.email).goAway(2000);
-          }
+      this.$toasted.show(error.response.data.email).goAway(2000);
+  }
           if (error.response.data.username) {
-            this.$toasted.show(error.response.data.username).goAway(2000);
-          }
+      this.$toasted.show(error.response.data.username).goAway(2000);
+  }
           if (error.response.data.password) {
-            this.$toasted.show(error.response.data.password).goAway(2000);
-          }
-        });
-    },
+      this.$toasted.show(error.response.data.password).goAway(2000);
+  }
+});
+},
     validateInput() {
       if (!this.username) {
-        this.$toasted.show(this.$t("SignPages.requireName")).goAway(2000);
-        return false;
-      }
+      this.$toasted.show(this.$t("SignPages.requireName")).goAway(2000);
+    return false;
+  }
       if (!this.email) {
-        this.$toasted.show(this.$t("SignPages.requireEmail")).goAway(2000);
-        return false;
-      }
+      this.$toasted.show(this.$t("SignPages.requireEmail")).goAway(2000);
+    return false;
+  }
       if (!this.password) {
-        this.$toasted.show(this.$t("SignPages.requirePassword")).goAway(2000);
-        return false;
-      }
+      this.$toasted.show(this.$t("SignPages.requirePassword")).goAway(2000);
+    return false;
+  }
       if (this.password.length < 8) {
-        this.$toasted
-          .show(this.$t("SignPages.requreValidPassword"))
-          .goAway(2000);
-        return false;
-      }
+      this.$toasted
+        .show(this.$t("SignPages.requreValidPassword"))
+        .goAway(2000);
+  return false;
+}
       if (this.confirm_password != this.password) {
-        this.$toasted
-          .show(this.$t("SignPages.requirePasswordCorrespondance"))
-          .goAway(2000);
-        return false;
-      }
+      this.$toasted
+        .show(this.$t("SignPages.requirePasswordCorrespondance"))
+        .goAway(2000);
+  return false;
+}
       let emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0, 61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0, 61}[a-zA-Z0-9])?)*$/gim;
       if (!this.email.match(emailRegex)) {
-        this.$toasted.show(this.$t("SignPages.requireValidEmail")).goAway(2000);
-        return false;
-      }
-      return true;
-    }
+      this.$toasted.show(this.$t("SignPages.requireValidEmail")).goAway(2000);
+    return false;
   }
+  return true;
+}
+}
 };
 </script>
 
-  <style lang="scss" scoped>
+<style lang="scss" scoped>
+
 @import "../assets/stylesheets/colors.scss";
 
 .content-button {
