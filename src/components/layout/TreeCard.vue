f6291b5b9bbdd074b504fd8e0f9cc65f52dd2dd2
Hugo Sobral <hugosobral10@gmail.com>
2019-11-13 17:14:08 -0300

    #120 add TreeCard component and finish property view

---
+++ src/components/layout/TreeCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div
+    class="tree-card"
+  >
+    <v-card
+      height="125"
+      width="265"
+      class="justify-right"
+    >
+      <v-btn
+        icon
+        color="#56A3A6"
+      >
+        <v-icon>mdi-pencil</v-icon>
+      </v-btn>
+    </v-card>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'TreeCard',
+  }
+</script>
+
+<style>
+  .tree-card{
+    height: 130px;
+    width: 265px;
+    border-radius: 12px;
+    box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+    margin-left: 30px;
+    background: white;
+  }
+
+  .justify-right{
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+  }
+</style>
--- src/views/Property.vue
+++ src/views/Property.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     id="property-view"
-    style="background-color:#FDF7F7"
+    class="property-view"
   >
     <div
       class="top-content"
@@ -15,6 +15,7 @@
 
       <div
         class="justify-left header-content"
+        id="username"
       >
         <h2>
           Nome
@@ -22,6 +23,7 @@
         
         <div
           class="justify-left subheader-content"
+          id="user-info"
         >
         <a> Árvores </a>
         <a> Propriedades </a>
@@ -33,32 +35,62 @@
     <div
       id="trees-list"
     >
-      <v-sheet
-        height="260"
-        style="background-color:#FDF7F7"
+
+      <div
+        class="slider-sheet"
+        id="slider-sheet"
       >
-
         <div
-          class="justify-left"
-          style="margin-left:30px"
+          class="justify-left trees-title"
+          id="trees-title"
         >
-          <a> Funcionando </a>
+          <h3> Árvores </h3>
         </div>
-
-        <v-window>
+      
+        <v-window
+          style="margin-top:10px"
+        >
           <v-window-item
             v-for="n in 3"
             :key="n"
+            :value="n"
           >
-            <v-card
-              height="125"
-              width="265"
-              class="tree-card"
-            >
-            </v-card>
+            <TreeCard/>
           </v-window-item>
         </v-window>
-      </v-sheet>
+      </div>
+    </div>
+
+    <div
+      id="footer-content"
+      class="footer-content"
+    >
+      <div
+        id="footer-title"
+        class="justify-left footer-title"
+      >
+        
+        <a
+          class="title-style"
+        > 
+          Colheitas 
+        </a>
+      </div>
+
+      <v-list>
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon
+              color="red"
+            >
+              mdi-checkbox-blank-circle
+            </v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-title>Colheita babadeira</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    
     </div>
   </div>
 </template>
@@ -66,10 +98,14 @@
 <script>
   /* Components imports */
   import TopBar from '@/components/layout/TopBar'
+  import TreeCard from '@/components/layout/TreeCard'
+  import WeeklyCard from '../components/layout/WeeklyCard'
 
   export default {
     components: {
       TopBar,
+      TreeCard,
+      WeeklyCard,
     },
 
     data() {
@@ -83,8 +119,19 @@
 <style lang="scss" scoped>
 @import "../assets/stylesheets/colors.scss";
 
+  .property-view{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #FDF7F7;
+    display: flex;
+    flex-direction: column;
+  }
+
   .top-content{
-    height: 190px;
+    height: 350px;
     width: 100%;
     background-image: linear-gradient(
       45deg,
@@ -96,8 +143,8 @@
   }
 
   .header-content{
-    padding-top: 55px;
-    padding-left: 40px;
+    margin-top: 40px;
+    margin-left: 40px;
     color: white;
   }
 
@@ -106,17 +153,45 @@
     font-size: 14px;
   }
 
-  .tree-card{
-    background: #FFFFFF;
-    box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
-    border-radius: 12px !important;
-    margin-left: 30px;
+  .trees-title{
+    margin-left:30px;
+    margin-top: 40px;
+    color: #56A3A6;
   }
   
   .justify-left{
     display: flex;
     flex-direction: column;
     align-items: flex-start;
+  }
+
+  .footer-title{
+    width: 100%;
+    height: 300px;
+  }
+
+  .footer-content{
+    background: white;
+    margin-top: 50px;
+    height: 100%;
+    border-radius: 12px 12px 0px 0px;
+  }
+
+  .footer-title{
+    height: 50px;
+    border-radius: 12px 12px 0px 0px;
+    background-image: linear-gradient(
+      45deg,
+      rgba(86, 163, 166, 1),
+      rgba(75, 125, 170, 85),
+      rgba(75, 125, 170, 105)
+    );
+    color: white;
+    font-size: 18px;
+  }
+
+  .title-style{
+    margin: 12px 0px 0px 24px;
   }
 
 </style>
