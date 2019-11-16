6ad68826e6490dba359faf71654d0373eced7d46
Vítor Xoteslem <vitorcx010@gmail.com>
2019-11-16 06:09:12 -0300

    #178 Remove compose volumes and add logs
    Co-authored-by: Shayane <shayaneliebe@gmail.com>
    Co-authored-by: Babadeira <leonardodasigomes@gmail.com>

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -84,7 +84,9 @@ import TopBar from '@/components/layout/TopBar'
 import BottomBar from '@/components/layout/BottomBar'
 import SignButton from '@/components/input/SignButton'
 import router from '@/router'
-
+console.log(process.env.VUE_APP_API_URL)
+console.log("VUE_APP_MODE"+process.env.VUE_APP_MODE)
+console.log("NODE_ENV:"+process.env.NODE_ENV)
 export default {
   components: {
     TopBar,
